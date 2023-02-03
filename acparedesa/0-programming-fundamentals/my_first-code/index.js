e62b61c166063c0a8d965a2fb57401340a5a16de
acparedesa <ascaro482417@gmail.com>
2023-02-02 20:47:55 -0500

    feat: my_first_code

---
+++ acparedesa/0-programming-fundamentals/my_first-code/index.js
@@ -0,0 +1,8 @@
+var nombre = "Astrid Carolina"
+var apellido = "Paredes Angel"
+var cedula = "1152440842"
+var tipoDeSangre  = "B+"
+var altura = 1.60
+var esFemenino = true
+
+console.log(nombre, apellido, cedula, tipoDeSangre, altura, esFemenino)
