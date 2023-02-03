375cd7e0979460a177829ce93d22b3a4de1bfed7
JuanPabaz <jpgthf@gmail.com>
2023-02-02 19:13:56 -0500

    feat: create my first program

---
+++ JuanPabaz/0-programming-fundamentals/my_first_program/example.js
@@ -0,0 +1,3 @@
+console.log("Hola mundo")
+var caja="zapato"
+console.log(caja)
---
+++ JuanPabaz/0-programming-fundamentals/my_first_program/index.js
@@ -0,0 +1,8 @@
+var nombre = "Juan Felipe"
+var apellido = "Giraldo"
+var id = "1032010343"
+var tipoDeSangre = "A+"
+var altura = 1.70
+var esFemenino = true
+
+console.log(nombre,apellido,id,tipoDeSangre,altura,esFemenino)
