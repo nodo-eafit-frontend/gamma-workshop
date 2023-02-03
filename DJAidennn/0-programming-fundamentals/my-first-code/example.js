dbf67201474a24bb0d5baec20b5e1eaa9289204b
DJAidennn <samualvarezh@gmail.com>
2023-02-02 19:13:51 -0500

    feat: create my first code

---
+++ DJAidennn/0-programming-fundamentals/my-first-code/example.js
@@ -0,0 +1,5 @@
+console.log ("hello")
+var box = "shoe"
+console.log (box)
+
+
---
+++ DJAidennn/0-programming-fundamentals/my-first-code/index.js
@@ -0,0 +1,9 @@
+var name = "Samuel"
+var lastName = "Alvarez"
+var cedula = "32412341"
+var typeOfBlood = "o+"
+var height = 1.82
+var isMale = true
+
+console.log (name,lastName,cedula,typeOfBlood,height,isMale) 
+
