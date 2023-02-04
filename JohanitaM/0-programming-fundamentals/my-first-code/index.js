a23060c1c906536817843b78307fd870b272b4d2
JohanitaM <jole360@hotmail.es>
2023-02-03 21:29:33 -0500

    feat: create my first code

---
+++ JohanitaM/0-programming-fundamentals/my-first-code/index.js
@@ -0,0 +1,8 @@
+var nombre = "Leydy" 
+var apellido = "Mosquera"
+var cedula = "12345678"
+var tipoDeSangre = "0+"
+var altura = 1.60
+var esFemenino = true
+
+console.log(nombre,apellido,cedula,tipoDeSangre,altura,esFemenino)
