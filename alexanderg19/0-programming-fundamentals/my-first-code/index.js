0424de44349499276edfb2f541778bd9007b38ec
Alexander <alexander.gallol1020@gmail.com>
2023-02-07 19:14:52 -0500

    feat: Add new folder alexanderg19

---
+++ alexanderg19/0-programming-fundamentals/my-first-code/index.js
@@ -0,0 +1,9 @@
+let nombre = "Alexander"
+let apellido = "Gallo Lopez"
+let cedula = "3204340584209"
+let estatura = 1.80
+let lugarDeExpedicion = "bogotagit" 
+let tipoDeSangre = "A-"
+let genero = "Masculino"
+
+console.log(nombre, apellido, cedula, estatura, lugarDeExpedicion, tipoDeSangre, genero)
