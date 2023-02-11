7286852add328b35d9277c8bde1e9218bd84bd97
JohanitaM <jole360@hotmail.es>
2023-02-10 21:27:25 -0500

    feat :conditionals and block-scope

--- JohanitaM/0-programming-fundamentals/my-first-code/operacion-aritmetica.js
+++ JohanitaM/0-programming-fundamentals/my-first-code/operacion-aritmetica.js
@@ -9,24 +9,18 @@ console.log("la suma de", num1, "+", num2,);
 console.log(resultadoSuma);
 
 // Resta
-let num3 = 75;
-let num4 = 8;
-let resultadoResta = num3 - num4;
-console.log("la resta de", num3, "-", num4,);
+let resultadoResta = num1 - num2;
+console.log("la resta de", num1, "-", num2,);
 console.log(resultadoResta);
 
 // Multiplicar
-let num5 = 75;
-let num6 = 8;
-let resultadoMultiplicar = num5 * num6;
-console.log("la multiplicación de", num5, "+", num6,);
+let resultadoMultiplicar = num1 * num2;
+console.log("la multiplicación de", num1, "+", num2,);
 console.log(resultadoMultiplicar);
 
 // Dividir
-let num7 = 75;
-let num8 = 8;
-let resultadoDividir = num7 / num8;
-console.log("la división de", num7, "+", num8,);
+let resultadoDividir = num1 / num2;
+console.log("la división de", num1, "+", num2,);
 console.log(resultadoDividir);
 
 // Módulo
@@ -34,12 +28,17 @@ let resultadoMódulo = num1 % num2;
 console.log("La Módulo de", num1, "%", num2)
 console.log(resultadoMódulo);
 
-///////
-let numero = 90;
+// Es par
+let numero = 21;
 let esPar = numero % 2 === 0;
 console.log("este número es par", numero);
 console.log(esPar); 
 
+// es impar
 let esImpar = numero % 2 !== 0; 
 console.log("este número es Impar", numero);
 console.log(esImpar);
+
+//Es impar
+console.log("Este numero es impar (Negación), numero");
+console.log(!esPar);
