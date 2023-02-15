7c381890c30d8478d774616bfd7e43185e3d0afd
DJAidennn <samualvarezh@gmail.com>
2023-02-14 21:45:13 -0500

    feat: Add fast test game

---
+++ DJAidennn/0-programming-fundamentals/my-first-code/block-scope.js
@@ -0,0 +1,23 @@
+// Deporte mundial
+let deporte = 'futbol';
+
+{
+    let deporteLocal = 'rugby';
+    console.log ('Deporta nacional:', deporteLocal);
+}
+
+// USA
+
+{
+    let deporteLocal = 'futbolAmericano';
+    console.log('Deporte USA local:', deporteLocal);
+
+    // Estado
+    {
+        let deporteLocal = 'basketball';
+        console.log('Deporte de estado:', deporteLocal)
+    }
+}
+
+// Ver en pantalla
+console.log('Deporte global:', deporte)
