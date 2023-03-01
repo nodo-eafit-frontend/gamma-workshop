ba36efe090dabd05178801a606faccdb5e0433fd
kellozcoy <kjorozcoy@eaafit.edu.co>
2023-03-01 06:17:32 -0500

    para magazine

---
+++ kellozcoy/0-programming-fundamentals/my_first_code/conditionals.js
@@ -0,0 +1,67 @@
+console.clear();
+
+//tienda de Transporte
+
+let medioDeTransporte = "carro";
+
+if (loQuiero) {
+
+    medioDeTransporte = medioDeTransporte 
+    console.clear();
+// Yo
+let presupuesto = 20_000;
+
+// Tienda de transportelet medioDeTrasporte = '🚗';
+let costoCarro = 50_000;
+// Compraré si tengo el presupuesto y el transporte exiteif () {
+  medioDeTrasporte = medioDeTrasporte + '     comprado!';
+} else {
+  medioDeTrasporte = medioDeTrasporte + '     😥!';
+}
+console.log(medioDeTrasporte); 
+
+
+console.clear();
+// Yolet presupuesto = 20_000;
+// Tienda de transportelet medioDeTrasporte = '';
+let costoCarro = 30_000;
+let prestamoBanco = 10_000;
+// Compraré si 'tengo el presupuesto' y el transporte exiteif (medioDeTrasporte && costoCarro <= presupuesto) {
+  medioDeTrasporte = medioDeTrasporte + '     comprado!';
+{}  if (false) {
+  // Pedir prestamo de 10_000 que alcance para comprar el carro  medioDeTrasporte = medioDeTrasporte + '     Prestamo!' + ' ' + presupuesto + prestamoBanco;
+} else {
+  medioDeTrasporte = medioDeTrasporte + '     😥!';
+}
+console.log(medioDeTrasporte); 
+
+/// Condicionales con switchlet diaActual = 'jueves';
+let mensaje = '';
+if (diaActual === 'lunes') {
+  // lunes -> Estudiar Interactiva  mensaje = 'Estudiar Interactiva';
+} else if (diaActual === 'martes') {
+  // martes -> Tenemos clase de programación  mensaje = 'Tenemos clase de programación';
+} else if (diaActual === 'miercoles') {
+  // miercoles -> Tenemos clase de programación  mensaje = 'Tenemos clase de programación';
+} else if (diaActual === 'jueves') {
+  // jueves -> Tenemos clase de programación  mensaje = 'Tenemos clase de programación';
+} else if (diaActual === 'viernes') {
+  // viernes -> Ir a farrear  mensaje = 'Ir a farrear';
+} else if (diaActual === 'sabados') {
+  // sabados -> Limpiamos hogar  mensaje = 'Limpiamos hogar';
+} else {
+  // por defecto -> descansar  mensaje = 'descansar';
+}
+console.log('\nQue haré', diaActual, mensaje);
+
+let letra ='A';
+//switch
+switch (diaActual) {
+    case value:
+        
+        
+        break;
+
+    default:
+        break;
+}
---
+++ kellozcoy/0-programming-fundamentals/my_first_code/operacion-aritmetica.js
@@ -0,0 +1,9 @@
+let numero = 20;
+let esPar = numero % 2 === 0;
+console.log("este número es par", numero);
+console.log(esPar);
+
+let esImpar = numero % 2 !== 0;
+
+console.log("este número es impar", numero);
+console.log(esImpar);
