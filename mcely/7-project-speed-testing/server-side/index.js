62a10449e7197fcd7b6bd0af2733324cff907277
Marvin Cely <mardancely@gmail.com>
2023-03-07 19:14:32 -0500

    feat: INclude first serve node

--- mcely/7-project-speed-testing/server-side/index.js
+++ mcely/7-project-speed-testing/server-side/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+// API
+app.get('/', (req, res) => {
+  // Aquí obtener info del json
+});
+
 app.listen(port, () => {
   console.log('Servidor corriendo... 😀');
 });
