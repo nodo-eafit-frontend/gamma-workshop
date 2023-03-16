0f551930cd92a898e4b9febc121247be67bdb4ce
JohanitaM <jole360@hotmail.es>
2023-03-16 08:51:42 -0500

    Feat: Angular

---
+++ JohanitaM/7-project-speed-testing/client-side-angular/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { StatsComponent } from './pages/stats/stats.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent},
+  { path: 'stats', component: StatsComponent},
+];
+
+@NgModule({   // este es un decorador para asignarle las propiedades a la clase approutingmodule.
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ JohanitaM/7-project-speed-testing/client-side-angular/src/app/services/main.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainService {
+  private response: any;
+  private url: string = 'http://localhost:3001/';
+  constructor(private http: HttpClient) {}
+
+  getMainInfo(): any {
+    // this.http.get(url);
+
+    return { dummy: 'dummy' };
+  }
+}
