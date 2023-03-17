7684eb8356665b4de37539a41db529333c3b5313
Alexander <alexander.gallol1020@gmail.com>
2023-03-17 11:51:36 -0500

    feat: Include angular with components and services

---
+++ alexanderg19/7-project-speed-testing/client-side-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageModule } from './page/page.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PageModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ alexanderg19/7-project-speed-testing/client-side-angular/src/app/services/main.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainService {
+  private response: any;
+  private url: string = 'http://localhost:3001/';
+  constructor(private http: HttpClient) { 
+     
+  }
+
+  getMainInfo(): any {
+    
+    return this.http.get(this.url);
+  }
+}
