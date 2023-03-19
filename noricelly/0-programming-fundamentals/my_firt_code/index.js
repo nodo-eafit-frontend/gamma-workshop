6e757425bed7af50f48248946ac8792d4b1bb8a7
noricelly <nory1291@gmail.com>
2023-03-18 21:30:37 -0500

    feat: magazine-html-css

--- noricelly/0-programming-fundamentals/my_firt_code/index.js
+++ noricelly/0-programming-fundamentals/my_firt_code/index.js
@@ -1,9 +1,15 @@
-var nombre = "yesija"
-var apellido = "moreno"
-var cedula = "123456"
-var tipoDeSangre = "a-"
-var altura = 1.55
-var esFemenino = true
+let nombre = "yesijaclear"
+let apellido = "moreno"
+let cedula = "123456"
+let tipoDeSangre = "a-"
+let altura = 1.55
+let edad = 18
+let esFemenino = true
 
 console.log(nombre,apellido,cedula,tipoDeSangre,altura,esFemenino)
 
+
+
+console.log("eres mayor de edad");
+let respuesta = 18 <= edad;
+console.log(respuesta);
