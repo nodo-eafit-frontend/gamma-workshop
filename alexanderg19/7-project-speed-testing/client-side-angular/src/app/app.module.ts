530ab4faa9da7340cab21f660ad5bb40f7800b50
Alexander <alexander.gallol1020@gmail.com>
2023-03-20 16:16:52 -0500

    feat: Include module materials

--- alexanderg19/7-project-speed-testing/client-side-angular/src/app/app.module.ts
+++ alexanderg19/7-project-speed-testing/client-side-angular/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {MaterialModule} from './material.module';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageModule } from './page/page.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StatsComponent } from './page/stats/stats.component';
+import { MainComponent } from './page/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StatsComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    PageModule,
+    BrowserAnimationsModule,
+    MaterialModule,
 
   ],
   providers: [],
