92551db2761cea298ab8138b3fb252b3a40e25f7
Marvin Cely <mardancely@gmail.com>
2023-04-12 00:14:00 -0500

    feat: incude responsive vite

--- alexanderg19/7-project-speed-testing/client-side-react/src/index.js
+++ alexanderg19/7-project-speed-testing/client-side-react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 const saludar = (nombre) => `Hola ${nombre}, ¿Cómo estás?`;
